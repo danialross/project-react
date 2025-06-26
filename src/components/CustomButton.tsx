5faf51c030f40c1d60c2f781bf5aa344b63b29ab
danialross <danialrossar@gmail.com>
2025-06-26 21:28:08 +0800

    add button animation

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -17,7 +17,7 @@ export default function CustomButton({
       onClick={onClick}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
-      className={`${className} bg-primary w-full h-14 rounded-lg hover:text-lg focus:outline-none focus:ring-tertiary focus:ring-4 transition-all duration-300 ease-in-out `}
+      className={`${className} bg-primary w-full h-14 hover:scale-105 rounded-lg outline-none focus:outline-none focus:ring-tertiary focus:ring-4 transition-all duration-300 ease-in-out `}
     >
       {children}
     </button>
