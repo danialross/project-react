56b6e78f18f04e1eb6f702b69ba82f59bc8ad491
danialross <danialrossar@gmail.com>
2025-06-27 01:05:41 +0800

    Intro animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import LogoBanner from "./components/LogoBanner";
+import Intro from "./components/Intro";
 
 function App() {
-	const [isRegistering, setIsRegistering] = useState(false);
-	return (
-		<div
-			className="relative bg-secondary w-screen h-screen grid grid-cols-2">
-			<Login setRegistering={setIsRegistering}/>
-			<Register setRegistering={setIsRegistering}/>
-			<LogoBanner
-				className={`absolute transition-transform duration-300 ease-in-out ${isRegistering ? 'translate-x-0' : 'translate-x-full'} `}/>
-
-		</div>
-	);
+  const [isRegistering, setIsRegistering] = useState(false);
+  return (
+    <div className="relative bg-secondary w-screen h-screen grid grid-cols-2">
+      <Login setRegistering={setIsRegistering} />
+      <Register setRegistering={setIsRegistering} />
+      <LogoBanner
+        className={`absolute transition-transform duration-300 ease-in-out ${isRegistering ? "translate-x-0" : "translate-x-full"} `}
+      />
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,45 @@
-export default function Intro() {
-	return <div className={"w-screen h-screen flex items-center justify-center"}>
+import { useEffect, useState } from "react";
 
-	</div>
+export default function Intro() {
+  const [isOverlayVisible, setIsOverlayVisible] = useState(true);
+  const [isShowingLogo, setIsShowingLogo] = useState(false);
+  const [isHidingLogo, setIsHidingLogo] = useState(false);
+
+  useEffect(() => {
+    const showingTimeout = setTimeout(() => {
+      setIsShowingLogo(true);
+    }, 300);
+    const overlayTimeout = setTimeout(() => {
+      setIsOverlayVisible(false);
+    }, 3000);
+    return () => {
+      clearTimeout(showingTimeout);
+      clearTimeout(overlayTimeout);
+    };
+  }, []);
+
+  useEffect(() => {
+    const hidingTimeout = setTimeout(() => {
+      setIsHidingLogo(true);
+    }, 2000);
+    return () => {
+      clearTimeout(hidingTimeout);
+    };
+  }, [isShowingLogo]);
+
+  return (
+    <div
+      className={`absolute bg-tertiary z-20 w-screen h-screen flex items-center justify-center ${isOverlayVisible ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-opacity duration-700 ease-in-out`}
+    >
+      <div className={"relative w-1/3 "}>
+        <div
+          className={`absolute ${isShowingLogo ? "translate-x-full" : "translate-x-0"} top-0 w-full h-full outline outline-4 outline-tertiary bg-tertiary transition-transform duration-700 ease-in-out`}
+        />
+        <div
+          className={`absolute ${isHidingLogo ? "translate-x-0" : "-translate-x-full"} top-0 w-full h-full outline outline-4 outline-tertiary bg-tertiary transition-transform duration-700 ease-in-out`}
+        />
+        <img src={"/Logo.webp"} className={"object-contain"} />
+      </div>
+    </div>
+  );
 }
